0d657191481c57c7f3465b6cdaea852c8c0535d0
chen <yurenchen@yeah.net>
2026-08-09 17:14:25 +0800

    U src/websocket.js: add loading stat handle

--- client/src/websocket.js
+++ client/src/websocket.js
@@ -9,6 +9,7 @@ export default {
             roomInput: '',
             roomDialog: false,
             retry: 0,
+            receivedLoading: true,
             event: {
                 //add to latest
                 receive: data => {
@@ -17,8 +18,9 @@ export default {
                 //add to latest, data item old first
                 receiveMulti: data => {
                     console.log('--- render first page directly')
-                    this.$root.received.unshift(...Array.from(data).reverse());
-                    //this.appendChunked(Array.from(data).reverse(), 'unshift');
+                    this.receivedLoading = true;
+                    // this.$root.received.unshift(...Array.from(data).reverse());
+                    this.appendChunked(Array.from(data).reverse(), 'unshift');
                 },
                 //add to oldest, data item old first
                 receiveMultiOld: data => {
@@ -26,6 +28,7 @@ export default {
                     //--- improve ui stuck: load large list by chunk
                     console.log('--- render other pages by chunk')
                     this.appendChunked(Array.from(data).reverse(), 'push');
+                    this.receivedLoading = false; ///loading done
                 },
                 revoke: data => {
                     let index = this.$root.received.findIndex(e => e.id === data.id);
@@ -35,6 +38,7 @@ export default {
                 config: data => {
                     this.retry = 0; //real success login
                     this.$root.config = data;
+                    this.receivedLoading = true;
                     console.log(
                         `%c Cloud Clipboard ${data.version} by TransparentLC %c https://github.com/TransparentLC/cloud-clipboard `,
                         'color:#fff;background-color:#1e88e5',
@@ -160,10 +164,12 @@ export default {
         cancelChunkFrame() {
             cancelAnimationFrame(this._chunkFrame);
             this._chunkFrame = null;
+            this.receivedLoading = false;
         },
         appendChunked(items, mode = 'push', chunkSize = 2000) {
             this.cancelChunkFrame();
             if (!Array.isArray(items) || !items.length) return;
+            this.receivedLoading = true;
             const received = this.$root.received;
             const chunks = [];
             for (let i = 0; i < items.length; i += chunkSize) {
@@ -180,6 +186,8 @@ export default {
                 }
                 if (index < chunks.length) {
                     this._chunkFrame = requestAnimationFrame(step);
+                } else {
+                    // this.receivedLoading = false;
                 }
             };
             step();
